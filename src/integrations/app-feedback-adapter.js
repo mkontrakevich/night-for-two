388d13c198844f21f3752f85c9c14dc341de2999
mkontrakevich <m.kontrakevich@gmail.com>
2026-09-25 17:28:31 +0300

    feat: decouple bot journal feedback

---
+++ src/integrations/app-feedback-adapter.js
@@ -0,0 +1,9 @@
+export function createLoveStoryEventFeedback(){
+  return {
+    init:async()=>true,
+    submit:async()=>null,
+    saveToDay:async()=>null,
+    dismissDayShare:async()=>null,
+    get:async()=>null
+  };
+}
